34765f88ca6758bce6a224ad3b8b0f19bb7b3a7d
ziqianp0918 <ziqianp0918@hstat.org>
2017-03-03 18:55:36 +0000

    finish

--- models/model.rb
+++ models/model.rb
@@ -1,7 +1,8 @@
 require 'lyricfy'
 require 'rspotify'
+require 'pp'
 
-RSpotify.authenticate("client-id", "client-secret")
+RSpotify.authenticate(ENV["SPOTIFY_ID"],ENV["SPOTIFY_SECRET"])
 
 class Song
     attr_reader :name, :artist, :album_img, :lyrics
@@ -37,9 +38,12 @@ class Playlist
         playlist = RSpotify::Playlist.find(@user,@playlist_id)
         
         # get playlist name
-        
+        @name = playlist.name
     
         # fill the @songs array with new instances of the Song class
+       playlist.tracks.each do |track|
+        @songs << Song.new(track.name,track.artists.first.name,track.album.images.first['url'])
+        end
         
     end
     
@@ -53,7 +57,7 @@ end
 
 # use this to test out the API
 spotify_top_tracks_playlist = RSpotify::Playlist.find("spotify","5FJXhjdILmRA2z5bvz4nzf")
-puts spotify_top_tracks_playlist
+#pp spotify_top_tracks_playlist.album.images.first['url']
 
 # use this to test out the classes
 # spotify_top_tracks = Playlist.new("spotify","5FJXhjdILmRA2z5bvz4nzf")
